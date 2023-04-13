213d3a961a860eaf7a3815ca1894088f40d3c428
Wallaf Oliveira <106490011+WallafOliveira@users.noreply.github.com>
2023-04-13 13:44:07 -0300

    aula 13-04-23 programa testes

--- Index.js
+++ Index.js
@@ -1,1 +1,30 @@
+const entrada = require('prompt-sync')();   
+
 console.log('hello word!');
+
+let nome = entrada('Digite seu nome:');
+
+console.log('Olá, ' + nome);
+
+let  idadeTxt = entrada (nome +' Quantos anos voce tem?');
+let idade =  parseInt(idadeTxt);
+
+let resposta = 'n'
+if(idade <15) { 
+    resposta = entrada('Acredito que você esteja no fundamental .\nEstou Certo? (s/n)');
+}
+else if( idade >= 15  && idade <= 18){
+    resposta = entrada('Acredito que voce esteja cursando o ensino medio. \nEstou Certo? (s/n)')
+}
+else{
+    resposta = entrada ('Acredito que voce já tenha terminado o ensino medio. \nEstou Certo? (s/n) ')
+}
+
+if (resposta === 'n') {
+    console.log('Ok, eu não sei de tudo');
+}
+else{
+    console.log('Legal, quando eu crescer vou ser igual o Chat GPT');
+}
+
+entrada('Fim do programa, pressione ENTER para encerrar');
